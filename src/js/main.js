8ba737393738a6d60c3febfa9b97005110d02d39
ar4ibais <ak.hutov@yandex.ru>
2022-10-05 01:37:17 +0400

    made desktop version for index page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import sliders from "./modules/sliders";
 import popular from "./modules/popular";
+import Fancybox from "@fancyapps/ui";
 
 sliders();
 popular();
--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -12,6 +12,22 @@ const sliders = () => {
     $('.popular__slider').slick({
         slidesToShow: 4
     });
+
+    $('.reviews__slider').slick({
+        slidesToShow: 4
+    });
+
+    $('.stats__slider').slick({
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+
+    $('.partners__slider').slick({
+        slidesToShow: 6,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
 };
 
 export default sliders;
