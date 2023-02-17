8d827ad22233b0173dd837c66a189b3718d928c7
ar4ibais <ak.hutov@yandex.ru>
2023-02-18 01:33:53 +0400

    fast-entry block made an adaptive

--- src/js/modules/header-btn.js
+++ src/js/modules/header-btn.js
@@ -8,12 +8,14 @@ const headerBtn = () => {
         headerWrapper.classList.add('header__mobile-wrapper--active');
         header.classList.add('hide');
         header.classList.remove('show-block');
+        document.documentElement.style.overflow = 'hidden';
     });
 
     headerCloseBtn.addEventListener('click', () => {
         headerWrapper.classList.remove('header__mobile-wrapper--active');
         header.classList.add('show-block');
         header.classList.remove('hide');
+        document.documentElement.style.overflow = '';
     });
 };
 
