8b2039cf3106994a3b476514153fcbbd2f05cb9d
ar4ibais <ak.hutov@yandex.ru>
2023-02-19 15:58:10 +0400

    made adaptive for many sections

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -4,13 +4,47 @@ import 'slick-carousel';
 const sliders = () => {
     $('.hots__slider').slick({
         slidesToShow: 3,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1320,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
     $('.season__slider').slick();
 
     $('.popular__slider').slick({
-        slidesToShow: 4
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 970,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 690,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
     $('.reviews__slider').slick({
